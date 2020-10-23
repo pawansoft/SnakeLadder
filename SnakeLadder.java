ebfaadc512a443c98176a0fbc175fd04cdcb9ab8
Pawan Kumar <pk.soft29@gmail.com>
2020-10-23 18:47:31 +0530

    Added : start position and number of player

---
+++ SnakeLadder.java
@@ -0,0 +1,9 @@
+public class SnakeLadder {
+    final static int winPoint=100;
+    final static int startPosition=0;
+    final static int NumberOfPlayer=0;
+    public static void main(String[] args) {
+
+
+    }
+}
