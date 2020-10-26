c6edb06232b90d36aa391d0d9303fa0a981132d1
pawansoft <46833870+pawansoft@users.noreply.github.com>
2020-10-26 11:59:42 +0530

    Update SnakeLadder.java

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,7 +1,7 @@
 public class SnakeLadder {
     final static int winPoint=100;
     final static int startPosition=0;
-    final static int NumberOfPlayer=0;
+    final static int NumberOfPlayer=1;
     public static void main(String[] args) {
 
 
